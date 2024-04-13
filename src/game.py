da493d0e3b9bbf9c8f2428427ef889610f7eda65
Paul <ukpaulbd@gmail.com>
2024-04-13 03:11:39 -0400

    Tiger can kill

--- src/game.py
+++ src/game.py
@@ -3,21 +3,17 @@ from board import Board
 from constants import *
 import random
 
-
-
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.goats = []
-        self.tigers = [(0, 0), (0, BOARD_SIZE), (BOARD_SIZE, 0), (BOARD_SIZE, BOARD_SIZE)]
+        self.tigers = [(0, 0), (0, BOARD_SIZE-1), (BOARD_SIZE-1, 0), (BOARD_SIZE-1, BOARD_SIZE-1)]
         self.board = Board(screen)
         self.selected_tiger = None  # This will store the position of the selected tiger
         self.remaining_goat_number = 16
         self.goats_on_board = 0
         self.number_of_moves = 0
         self.needs_update = True  # Flag to track when the screen needs to be updated
-
-
 
     def place_goat(self):
         empty_positions = [(row, col) for row in range(BOARD_SIZE)
@@ -26,23 +22,48 @@ class Game:
         if empty_positions:
             new_goat_position = random.choice(empty_positions)
             self.goats.append(new_goat_position)
-            self.needs_update = True  # Set the flag to update the screen
             self.goats_on_board += 1
+            self.needs_update = True  # Set the flag to update the screen
+
+    def is_goat_in_path(self, old_pos, new_pos):
+        path = self.calculate_path(old_pos, new_pos)
+        for pos in path:
+            if pos in self.goats:
+                return True, pos
+        return False, None
+
+    def calculate_path(self, start, end):
+        path = []
+        start_row, start_col = start
+        end_row, end_col = end
+        row_step = (end_row - start_row) // max(abs(end_row - start_row), 1)
+        col_step = (end_col - start_col) // max(abs(end_col - start_col), 1)
+
+        current_row, current_col = start_row + row_step, start_col + col_step
+        while (current_row, current_col) != end:
+            path.append((current_row, current_col))
+            current_row += row_step
+            current_col += col_step
+
+        return path
 
     def handle_click(self, pos):
         x, y = pos[0] - MARGIN, pos[1] - MARGIN
         col, row = round(x / CELL_SIZE), round(y / CELL_SIZE)
-        # Check if the click is within bounds
         if 0 <= col <= BOARD_SIZE and 0 <= row <= BOARD_SIZE:
+            new_position = (row, col)
             if self.selected_tiger:
-                # Move tiger to the new location if it's a valid move
-                new_position = (row, col)
                 if new_position not in self.goats and new_position not in self.tigers:
-                    # Update the tiger's position
                     self.tigers.remove(self.selected_tiger)
                     self.tigers.append(new_position)
-                    self.number_of_moves +=1
+                    goats_in_path, goat_pos = self.is_goat_in_path(self.selected_tiger, new_position)
+                    print(goat_pos)
+                    if goats_in_path:  # If there are goats in the path, remove the first one
+                        self.goats.remove(goat_pos)
+                        self.goats_on_board -= 1
+                        self.remaining_goat_number -=1
                     self.selected_tiger = None
+                    self.number_of_moves += 1
                     self.needs_update = True  # Update screen to show selected tiger
 
                     # After moving tiger, place a goat randomly
@@ -51,9 +72,6 @@ class Game:
                 # Check if a tiger is clicked
                 if (row, col) in self.tigers:
                     self.selected_tiger = (row, col)
-
-
-
 
     def run(self):
         running = True
@@ -64,8 +82,6 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    self.board.draw_info(self.goats_on_board, self.remaining_goat_number, self.number_of_moves)
-                    pygame.display.flip()
                     self.handle_click(pygame.mouse.get_pos())
 
             if self.needs_update:  # Only draw when needed
@@ -75,4 +91,5 @@ class Game:
                 pygame.display.flip()  # Update the display
                 self.needs_update = False  # Reset the update flag
 
-    pygame.quit()
+        pygame.quit()
+
