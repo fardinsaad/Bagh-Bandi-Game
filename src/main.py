b8170d7c7c675f4c9d5d3f62a954edfc897cbf13
Paul <ukpaulbd@gmail.com>
2024-04-30 01:30:25 -0400

    Commenting code

--- src/main.py
+++ src/main.py
@@ -5,14 +5,16 @@ from constants import WINDOW_SIZE
 
 
 def main():
+    # Command line argument for passing the Algorithm name
     algorithm = sys.argv[1].lower()
-    print(algorithm)
     if algorithm not in ["random", "bfs", "dfs", "monte_carlo", "astar"]:
         sys.exit("Invalid Algorithm specified, Valid Options: random, bfs, dfs, astar, monte_carlo")
+    # Initialize our game
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode(WINDOW_SIZE)
     pygame.display.set_caption('Bagh Bandi Game')
+    # Passing the algorithm name and run the name
     game = Game(screen, algorithm)
     game.run()
     sys.exit()
